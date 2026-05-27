ac1a05379957d45b58e1ba345d96e6965ea93620
Jemson <jemsonmislani@gmail.com>
2026-05-27 11:52:52 +0800

    Install axios. Create api endpoint to server. Create useEffect function to get data. create handleaddtask function to validate inputs and post method to send data to server.

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
+const TaskModel = require('./models/Tasks')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/tasks')
+mongoose.connect('mongodb://127.0.0.1:27017/task_fullstack')
 .then(result => console.log('Backend is running!'))
 .catch(err => console.log(err))
 
-PORT = 3005
+app.get('/', (req, res) => {
+    TaskModel.find({})
+    .then(tasks => res.json(tasks))
+    .catch(err => res.json(err))
+})
+
+app.post('/create', (req, res) => {
+    TaskModel.create(req.body)
+    .then(tasks => res.json(tasks))
+    .catch(err => res.json(err))
+})
+
+const PORT = 3005
 app.listen(PORT, () => {
     console.log('Jem your server is running!')
 })
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,29 +1,66 @@
 
 import { useState } from 'react';
 import './Home.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 export default function Dashboard(){
     const [todo, setTodo] = useState('')
     const [date, setDate] = useState('')
-    const [task, setTask] = useState([{
-        tasks: 'Jogging', date: '05-25-2026'
-    }])
+    const [task, setTask] = useState([])
 
+    useEffect(() => {
+        axios.get('http://localhost:3005/')
+        .then(result => setTask(result.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleAddTask = () => {
+        if(!todo || !date){
+            alert('Please fill out fields')
+            return;
+        }
+
+        axios.post('http://localhost:3005/create', 
+            { todo, date }
+        )
+        .then(result => {
+            console.log(result.data)
+            setTask([...task, result.data])
+        })
+        .catch(err => console.log(err))
+
+        setTodo('')
+        setDate('')
+    }
 
     return(
     <>
         <h3 className='home-element'>What's our task?</h3>
         <div className='input-element'>
-            <input type="text" placeholder="Enter task"/>
-            <input type="date" />
-            <button className='add-btn'>Add task</button>
+            <input 
+                type="text" 
+                placeholder="Enter task"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}/>
+            <input 
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                />
+            <button 
+                onClick={handleAddTask}
+                className='add-btn'>Add task</button>
         </div>
         <div className='task-status'>
             <div>
                 <h3 className='sts-pending'>Pending task</h3>
                 {
-                    task.map((t) => {
-                        return <div className='tasks'>
-                            <p>{`${t.tasks} • ${t.date}`}</p> 
+                    task.map((t, index) => {
+                        return <div 
+                            key={index}
+                            className='tasks'>
+                            <p>{t.todo} • {t.date}</p> 
                             <button>Edit</button>
                             <button>Completed</button>
                             <button>Remove</button>
