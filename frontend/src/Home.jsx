9b6b46bca63b17ea9ae5bba3f1d2ea622525b3fb
Jemson <jemsonmislani@gmail.com>
2026-05-27 10:31:15 +0800

    Create Home component and css, style them.

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,7 +1,14 @@
 
+import './Home.css';
 export default function Dashboard(){
     return(
         <>
+            <h3 className='home-element'>What's our task?</h3>
+            <div className='input-element'>
+                <input type="text" placeholder="Enter task"/>
+                <input type="date" />
+                <button className='add-btn'>Add task</button>
+            </div>
         </>
     );
 }
