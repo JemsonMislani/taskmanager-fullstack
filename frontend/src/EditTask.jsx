3325ee3b7ec67cdadfb42cb3c32f69f10b0898c0
Jemson <jemsonmislani@gmail.com>
2026-05-27 14:52:50 +0800

    create api endpoint updating a task. Create a function to pass and display data.

--- frontend/src/EditTask.jsx
+++ frontend/src/EditTask.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './EditTask.css'
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -10,6 +10,7 @@ export default function EditTask(){
     const {id} = useParams()
     const [todo, setTodo] = useState('')
     const [date, setDate] = useState('')
+    const nav = useNavigate()
 
     useEffect(() => {
         axios.get('http://localhost:3005/getTask/'+id)
@@ -21,13 +22,24 @@ export default function EditTask(){
         .catch(err => console.log(err))
     }, [id])
 
+    const updateTask = (e) => {
+        e.preventDefault();
+        axios.put('http://localhost:3005/updateTask/'+id, 
+            {todo, date}
+        )
+        .then(result => {
+            console.log(result)
+            nav('/')
+        })
+    }
+
     return(
         <>
             <div className='edit-elem'>
                 <h3 className='edit-task-elem'>Edit my task</h3>
                 <Link 
                     to='/'
-                    className='btn btn-primary'>Undo</Link>
+                    className='btn btn-primary'>Back</Link>
                 <div className='input-element-edit'>
                     <input 
                         className='edit-todo'
@@ -42,6 +54,7 @@ export default function EditTask(){
                         onChange={(e) => setDate(e.target.value)}
                         />
                     <button
+                        onClick={updateTask}
                         className='update-btn'>Update task</button>
                 </div>
             </div>
